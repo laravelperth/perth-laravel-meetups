ceaa4b3abe8af75a4ba0f9512351cedcfa6d7154
Glenn Forrest <glennjamesforrest@gmail.com>
2025-03-19 14:00:47 +0800

    feat: update banner for community build & hack days

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,5 @@
-@if (now()->isBefore('2025-02-24'))
-    <div class="flex justify-center items-center gap-x-6 overflow-hidden bg-primary py-3 px-6 sm:px-3.5">
-        <p class="text-base/5 font-semibold">
-            Perth Laravel PHP is having it's next meetup on Sunday 23 February 2025. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/306008137/" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
-        </p>
-    </div>
-@endif
+<div class="flex justify-center items-center gap-x-6 overflow-hidden bg-primary py-3 px-6 sm:px-3.5">
+    <p class="text-base/5 font-semibold">
+        Perth Laravel PHP is hosting monthly community build & hack days. <a href="https://www.meetup.com/perth-laravel-php-meetup/" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
+    </p>
+</div>
